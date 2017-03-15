f1426bc5d183ff77d936757e84e7946f78996d7c
mocellas <fernando.mocellin@gmail.com>
2017-03-15 19:22:34 +0000

    Changes on express_server.js based on Shortening URLs exercise

--- webServer2/express_server.js
+++ webServer2/express_server.js
@@ -6,8 +6,22 @@ app.set("view engine", "ejs");
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
+
+function stringGen(len)
+{
+  var text = " ";
+
+  var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < len; i++ )
+    text += charset.charAt(Math.floor(Math.random() * charset.length));
+
+  return text;
+}
+
+console.log(stringGen(6));
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -22,9 +36,24 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase};
   res.render("urls_show", templateVars);
+});
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  var randomURL = stringGen(6);
+  urlDatabase[randomURL] = req.body.longURL;
+
+  // console.log(req.body.longURL);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/hello", (req, res) => {
@@ -33,4 +62,11 @@ app.get("/hello", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  console.log(urlDatabase)
+  let longURL = urlDatabase[req.params.shortURL]
+  console.log(longURL);
+  res.redirect(longURL);
 });
